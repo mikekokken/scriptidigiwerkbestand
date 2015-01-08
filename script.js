13dc226339726b885a8f0ae2f65d2fe714bf12e1
Max Peeperkorn <maxify@me.com>
2015-01-08 19:03:58 +0100

    beveiliging als er...
    … een ongelijk aantal ankers en frames zijn

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ Array.max = function( array )
 */
 function loadEssay() 
 {
-    $('#the_essay').fadeIn("slow", function() 
+    $( '#the_essay' ).fadeIn( "slow", function() 
     {
-        $('#frame_wrapper').fadeIn("slow");
-        $.scrollTo( $('#the_essay'), 1000);
+        $( '#frame_wrapper' ).fadeIn("slow");
+        $.scrollTo( $('#the_essay'), 1000 );
         
         // deze if statement zorgt ervoor dat als je nog een keer op lees klikt die frames niet nog een keer gepositioneerd worden.
         if (firstTry)
@@ -33,13 +33,13 @@ function loadEssay()
     draggable frame functions
 */
 
-function selectFrame(element, stack)
+function selectFrame( element, stack )
 {
     var a = [];
 
-    $(stack).each(function ()
+    $( stack ).each(function ()
     {
-        a.push($(this).css("z-index"))
+        a.push( $(this).css( "z-index" ))
     });
     
     // console.log(a[0], a[1], a[2]);
@@ -47,7 +47,7 @@ function selectFrame(element, stack)
     var maxZ = Array.max(a);
     console.log(maxZ);
 
-    $(element).css({"z-index": maxZ + 1})
+    $( element ).css({ "z-index": maxZ + 1 })
 };
 
 $(function() 
@@ -59,37 +59,45 @@ $(function()
 /*
     snap elke frame aan een anker om de positie te bepalen
 */
-function snapToAnker() 
+function snapToAnker () 
 {
     var ankers = []; // array om de positie van elk anker in op te slaan
     var frames = []; // array om de oorspronkelijk positie van elk frame in op te slaan
     var nummer = 0;  // variable (om elk frame te kunnen selecteren in een loop)
 
-    $(".anker").each(function () 
+    $( ".anker" ).each( function () 
     {
         ankers.push( $(this).offset() );
     });
 
 
-    $(".draagbaar").each(function () 
+    $( ".draagbaar" ).each( function () 
     {
         frames.push( $(this).offset() );
     });
 
-    for (var i = 0; i < ankers.length; i++) 
+    if ( ankers.length == frames.length )
     {
-        nummer = i + 1;
-       
-        console.log( ankers[i].top + ", " + ankers[i].left );
-        console.log( frames[i].top + ", " + frames[i].left );
+        for ( var i = 0; i < ankers.length; i++ ) 
+        {
+            nummer = i + 1;
+           
+            // console.log( ankers[i].top + ", " + ankers[i].left );
+            // console.log( frames[i].top + ", " + frames[i].left );
 
-       $("#frame" + nummer).css(
-       {
-            "top": ankers[i].top + frames[i].top + "px",
-            "left": ankers[i].left + frames[i].left + "px",
-            "background-color": "rgb(" + getRandomRange(0, 255) + "," + getRandomRange(0, 255) + "," + getRandomRange(0, 255) + ")"  
-       });
+           $( "#frame" + nummer ).css(
+           {
+                "top": ankers[i].top + frames[i].top + "px",
+                "left": ankers[i].left + frames[i].left + "px",
+                "background-color": "rgb(" + getRandomRange(0, 255) + "," + getRandomRange(0, 255) + "," + getRandomRange(0, 255) + ")"  
+           });
+        }
+    } 
+    else
+    {
+        alert( "Er zijn niet een gelijk aantal ankers en frames.." )
     }
+    
 }
 /*
     smoooth scroll function
